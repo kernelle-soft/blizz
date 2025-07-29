7f336995b1f43e8b66d7475d2647ee55fc794a6e
Voxel <jeffhilton.code@gmail.com>
2025-07-28 20:28:13 -0600

    Update bump-version.sh

--- .github/run/bump-version.sh
+++ .github/run/bump-version.sh
@@ -79,5 +79,13 @@ cargo check --workspace
 
 # Commit the version bump
 echo "Committing version bump to git..."
+
+# Extract author info from Cargo.toml
+AUTHOR_LINE=$(grep -A 10 '^\[workspace\.package\]' Cargo.toml | grep '^authors = ' | sed 's/authors = \["//' | sed 's/"\]//')
+AUTHOR_NAME=$(echo "$AUTHOR_LINE" | sed 's/ <.*$//')
+AUTHOR_EMAIL=$(echo "$AUTHOR_LINE" | sed 's/.*<\(.*\)>.*/\1/')
+
+git config --local user.email "$AUTHOR_EMAIL"
+git config --local user.name "$AUTHOR_NAME"
 git add .
-git commit -m "$NEW_VERSION [skip ci]"
+git commit -m "chore: bump version to $NEW_VERSION [skip ci]"
