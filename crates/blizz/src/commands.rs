c2e42918d8023fcd0f8bbc5c35d33d47943e6530
Voxel <jeffhilton.code@gmail.com>
2025-07-30 15:48:25 -0600

    tidy

--- crates/blizz/src/commands.rs
+++ crates/blizz/src/commands.rs
@@ -171,11 +171,7 @@ pub fn delete_insight(topic: &str, name: &str, force: bool) -> Result<()> {
 
 #[cfg(feature = "neural")]
 fn index_insight(insight: &mut Insight, force: bool, client: &EmbeddingClient) -> Result<bool> {
-  let should_update = if force {
-    true
-  } else {
-    !insight::has_embedding(insight)
-  };
+  let should_update = if force { true } else { !insight::has_embedding(insight) };
 
   if !should_update {
     return Ok(false);
